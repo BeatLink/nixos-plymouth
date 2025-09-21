34fd108eec7b8471a1ba8c447cb5de260eb30458
BeatLink <git@beatlink.simplelogin.com>
2025-09-21 02:33:57 -0500

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,6 +2,6 @@
     description = "A NixOS Plymouth theme displaying a pulsating NixOS Logo";
 
     outputs = inputs: {
-        nixosModules.default = ./modules.nix;
+        nixosModules.default = ./default.nix;
     };
 }
