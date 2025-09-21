208e196facc2fb656d15a1098c584a845372d7a7
BeatLink <git@beatlink.simplelogin.com>
2025-09-21 04:18:21 -0500

    Update nix and plymouth

--- nixos-plymouth.nix
+++ nixos-plymouth.nix
@@ -2,20 +2,17 @@
     pkgs ? import <nixpkgs> { },
 }:
 pkgs.stdenv.mkDerivation {
-    pname = "nixos-plymouth-theme";
+    pname = "nixos-plymouth";
     version = "master";
 
     src = ./src;
-
-    dontUnpack = true;
-    dontBuild = true;
 
     installPhase = ''
         # Copy files
         INSTALL_PATH="$out/share/plymouth/themes/nixos-plymouth"
         mkdir -p $INSTALL_PATH
-        install -m 755 -vDt $INSTALL_PATH nixos-plymouth.plymouth
-        install -m 644 -vDt $INSTALL_PATH resources/*
+        cp -v nixos-plymouth.plymouth $INSTALL_PATH
+        cp -Rv resources $INSTALL_PATH
 
         # Fix path
         sed -i "s@\/usr\/@$out\/@" $INSTALL_PATH/nixos-plymouth.plymouth"
